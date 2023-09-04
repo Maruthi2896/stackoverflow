70c7bd6dd65d153363f3863fb44f77217fa75b3a
Maruthi K J <123438736+Maruthi2896@users.noreply.github.com>
2023-09-04 09:56:25 +0530

    Add files via upload

---
+++ src/components/stackmain.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Sidebar from './sidebar';
+import Maincomp from './maincomp';
+
+function Stackmain() {
+  return (
+    <div className='stack-index'>
+    <div className='stack-index-content'>
+        <Sidebar/>
+        <Maincomp/>
+       </div>
+    </div>
+  )
+}
+
+export default Stackmain
